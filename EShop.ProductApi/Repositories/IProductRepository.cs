a5ee552df847b4759fe537806c183cd01306608c
Luiz Gustavo <gugajoao1020@gmail.com>
2026-06-29 19:23:57 -0300

    feat: add product repository interface and implementation

--- EShop.ProductApi/Repositories/IProductRepository.cs
+++ EShop.ProductApi/Repositories/IProductRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace EShop.ProductApi.Repositories
+﻿using EShop.ProductApi.Models;
+
+namespace EShop.ProductApi.Repositories;
+
+public interface IProductRepository
 {
-    public interface IProductRepository
-    {
-    }
+    Task<IEnumerable<Product>> GetProducts();
+
+    Task<Product> GetById(int id);
+
+    Task<Product> Create(Product product);
+
+    Task<Product> Update(Product product);
+
+    Task<Product> Delete(int id);
 }
