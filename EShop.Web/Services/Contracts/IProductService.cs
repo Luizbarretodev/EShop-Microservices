0d82b5359071efd72f8920e423699c51d90fc53e
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-01 22:57:00 -0300

    feat: add ProductService Interface

--- EShop.Web/Services/Contracts/IProductService.cs
+++ EShop.Web/Services/Contracts/IProductService.cs
@@ -6,7 +6,7 @@ public interface IProductService
 {
     Task<IEnumerable<ProductViewModel>> GetAllProducts();
     Task<ProductViewModel> FindProductById(int id);
-    Task<ProductViewModel> CreateProduct();
-    Task<ProductViewModel> UpdateProduct();
+    Task<ProductViewModel> CreateProduct(ProductViewModel productVM);
+    Task<ProductViewModel> UpdateProduct(ProductViewModel productVM);
     Task<bool> DeleteProduct();
 }
