7a53f1b7c31d71bc4f3dfbb14edc993fc8876765
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-02 17:19:11 -0300

    feat: add CreateProduct implementation

--- EShop.Web/Models/ProductViewModel.cs
+++ EShop.Web/Models/ProductViewModel.cs
@@ -1,12 +1,19 @@
-﻿namespace EShop.Web.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Web.Models;
 
 public class ProductViewModel
 {
     public int Id { get; set; }
+    [Required]
     public string? Name { get; set; }
+    [Required]
     public decimal Price { get; set; }
+    [Required]
     public string? Description { get; set; }
+    [Required]
     public long Stock { get; set; }
+    [Required]
     public string? ImageURL { get; set; }
     public string? CategoryName { get; set; }
     public int? CategoryId { get; set; }
