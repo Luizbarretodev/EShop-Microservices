486d12fc3adee9c72db2480c44ef59baa806fdad
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-04 19:17:47 -0300

    feat: add ApplicationUser class

---
+++ EShop.IdentityServer/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EShop.IdentityServer.Data;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; } = String.Empty;
+    public string lastName { get; set; } = String.Empty;
+}
