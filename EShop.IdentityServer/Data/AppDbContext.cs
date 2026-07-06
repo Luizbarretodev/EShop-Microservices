d55782405d4ff21dbe87bc50007352f3f83679e7
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-06 20:21:15 -0300

    fix: deleting IdServer to implement JWT

--- EShop.IdentityServer/Data/AppDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace EShop.IdentityServer.Data;
-
-public class AppDbContext : IdentityDbContext<ApplicationUser>
-{
-    public AppDbContext(DbContextOptions<ApplicationUser> options) : base(options){ }
-}
--- EShop.IdentityServer/Data/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace EShop.IdentityServer.Data;
-
-public class ApplicationUser : IdentityUser
-{
-    public string FirstName { get; set; } = String.Empty;
-    public string lastName { get; set; } = String.Empty;
-}
