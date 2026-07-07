e2685b25688bdc6c5b2a6799a03caf3c81c479c3
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-06 21:23:31 -0300

    feat: configure program.cs and appsettins.json

--- EShop.AuthApi/Program.cs
+++ EShop.AuthApi/Program.cs
@@ -1,15 +1,24 @@
+using EShop.AuthApi.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configura o banco de dados com a connectionString
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// configura o aspnetcore com identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,6 +27,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// ativa os middlewares de auth, sempre autenticar e depois autorizar
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
