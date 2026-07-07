e017713d4e6e3b761c2ba02c41116cccecd19a4d
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-07 19:03:13 -0300

    feat: Add RegisterDto to AuthApi

---
+++ EShop.AuthApi/DTOs/RegisterDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.AuthApi.DTOs;
+
+public class RegisterDto
+{
+    [Required]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    public string Password { get; set; } = string.Empty;
+}
