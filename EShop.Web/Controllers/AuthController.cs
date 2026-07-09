f6838db42c64625fbcb2d2503943271e40038288
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-08 21:57:10 -0300

    feat: add AuthController to EshopWeb

---
+++ EShop.Web/Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using EShop.Web.Models;
+using EShop.Web.Services.Contracts;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace EShop.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _service;
+        public AuthController(IAuthService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid)
+                return View(loginVM);
+
+            var user = await _service.Login(loginVM);
+            if (user is null)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid email or password");
+                return View(loginVM);
+            }
+
+            var claims = new List<Claim>
+            {
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim("Token", user.Token)
+            };
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerVM)
+        {
+            if (!ModelState.IsValid) 
+                return View(registerVM);
+
+            var user = await _service.Register(registerVM);
+            if (!user)
+            {
+                ModelState.AddModelError(string.Empty, "Error to register user");
+                return View(registerVM);
+            }
+
+            return RedirectToAction(nameof(Login));
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
