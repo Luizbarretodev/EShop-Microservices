5cf0079d4af9148c92788aa535f3b59f8a951413
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-11 09:38:49 -0300

    feat: Add Models

---
+++ EShop.CartApi/Models/CartHeader.cs
@@ -0,0 +1,8 @@
+﻿namespace EShop.CartApi.Models;
+
+public class CartHeader
+{
+    public int Id { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public string CoupounCode { get; set; } = string.Empty;
+}
