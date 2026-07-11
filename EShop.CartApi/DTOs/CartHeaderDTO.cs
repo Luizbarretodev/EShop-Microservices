7dce9b488c142e008a023a77ec2b04a227391d4c
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-11 15:54:10 -0300

    feat: Add DTOs

---
+++ EShop.CartApi/DTOs/CartHeaderDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.CartApi.DTOs;
+
+public class CartHeaderDTO
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "User Id is required")]
+    public string UserId { get; set; } = string.Empty;
+    public string CoupounCode { get; set; } = string.Empty;
+}
---
+++ EShop.CartApi/DTOs/CartItemDTO.cs
@@ -0,0 +1,13 @@
+﻿using EShop.CartApi.Models;
+
+namespace EShop.CartApi.DTOs;
+
+public class CartItemDTO
+{
+    public int Id { get; set; }
+    public int Quantity { get; set; } = 1;
+    public int ProductId { get; set; }
+    public int CartHeaderId { get; set; }
+    public Product Product { get; set; } = new Product();
+    public CartHeader CartHeader { get; set; } = new CartHeader();
+}
