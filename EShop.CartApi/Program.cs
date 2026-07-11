500aadbc53af5e9f92157b24bd9e7e8a6d962f53
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-11 10:01:46 -0300

    feat(Cart): Add initial migrations

--- EShop.CartApi/Program.cs
+++ EShop.CartApi/Program.cs
@@ -1,4 +1,6 @@
+using EShop.CartApi.Context;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(
+            builder.Configuration.GetConnectionString("DefaultConnection")));
+});
 
 builder.Services.AddSwaggerGen(options =>
 {
