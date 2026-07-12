e25e0a332712172b24e947d8cea9fc5686851e61
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-12 16:23:23 -0300

    feat(Web): add ICartService

---
+++ EShop.Web/Services/ICartService.cs
@@ -0,0 +1,15 @@
+﻿using EShop.Web.Models;
+
+namespace EShop.Web.Services;
+
+public interface ICartService
+{
+    Task<CartViewModel> GetCartByUserId(int id);
+    Task<CartViewModel> AddItemToCartAsync(CartViewModel cartVM);
+    Task<CartViewModel> UpdateCartAsync(CartViewModel cartVM);
+    Task<bool> RemoveItemFromCartAsync(int id);
+
+    Task<bool> ApplyCouponAsync(CartViewModel cartVM, string coupon);
+    Task<bool> RemoveCouponAsync(int id);
+    Task<bool> CleanCartAsync(int id);
+}
