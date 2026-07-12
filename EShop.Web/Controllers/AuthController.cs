e75b00b66150281e5ffcef8d80afb7c9d3708f85
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-12 17:41:32 -0300

    feat(Web): add Add items to cart controller view

--- EShop.Web/Controllers/AuthController.cs
+++ EShop.Web/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using EShop.Web.Services.Contracts;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System.Data;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -40,14 +41,17 @@ namespace EShop.Web.Controllers
                 Console.WriteLine($"{claim.Type} = {claim.Value}");
             }
             var role = jwt.Claims.FirstOrDefault(c =>
-    c.Type == ClaimTypes.Role || c.Type == "role")?.Value ?? "Client";
+            c.Type == ClaimTypes.Role || c.Type == "role")?.Value ?? "Client";
+
+            var sub = jwt.Claims.FirstOrDefault(c => c.Type == "sub")?.Value ?? string.Empty;
 
             var claims = new List<Claim>
             {
-            new Claim(ClaimTypes.Name, user.UserName),
-            new Claim(ClaimTypes.Email, user.Email),
-            new Claim(ClaimTypes.Role, role),
-            new Claim("Token", user.Token)
+             new Claim(ClaimTypes.Name, user.UserName),
+             new Claim(ClaimTypes.Email, user.Email),
+             new Claim(ClaimTypes.Role, role),
+             new Claim("Token", user.Token),
+            new Claim(ClaimTypes.NameIdentifier, sub)
             };
 
             var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
@@ -65,7 +69,7 @@ namespace EShop.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel registerVM)
         {
-            if (!ModelState.IsValid) 
+            if (!ModelState.IsValid)
                 return View(registerVM);
 
             var user = await _service.Register(registerVM);
--- EShop.Web/Controllers/HomeController.cs
+++ EShop.Web/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
+using EShop.CartApi.Models;
 using EShop.Web.Models;
+using EShop.Web.Services;
 using EShop.Web.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace EShop.Web.Controllers
 {
@@ -9,11 +13,13 @@ namespace EShop.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IProductService _productService;
+        private readonly ICartService _cartService;
 
-        public HomeController(ILogger<HomeController> logger, IProductService productService)
+        public HomeController(ILogger<HomeController> logger, IProductService productService, ICartService cartService)
         {
             _logger = logger;
             _productService = productService;
+            _cartService = cartService;
         }
 
         public async Task<ActionResult<IEnumerable<ProductViewModel>>> Index()
@@ -26,6 +32,7 @@ namespace EShop.Web.Controllers
             return View(result);
         }
         [HttpGet]
+        [Authorize]
         public async Task<ActionResult<ProductViewModel>> ProductDetails(int id)
         { 
             var product = await _productService.FindProductById(id);
@@ -34,6 +41,43 @@ namespace EShop.Web.Controllers
                 return View("Error");
 
             return View(product);
+        }
+
+        [HttpPost]
+        [ActionName("ProductDetails")]  
+        [Authorize]
+        public async Task<ActionResult<ProductViewModel>> ProductDetailsPost(ProductViewModel productVM)
+        {
+            var userId = User.Claims
+                .FirstOrDefault(u => u.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            var cart = new CartViewModel
+            {
+                CartHeader = new CartHeaderViewModel
+                {
+                    UserId = userId
+                }
+            };
+
+            var cartItem = new CartItemViewModel
+            {
+                Quantity = productVM.Quantity,
+                ProductId = productVM.Id,
+                Product = await _productService.FindProductById(productVM.Id)
+            };
+
+            List<CartItemViewModel> cartItemsVm = new List<CartItemViewModel>(); 
+            cartItemsVm.Add(cartItem);
+            cart.CartItems = cartItemsVm;
+
+            var result = await _cartService.AddItemToCartAsync(cart);
+
+            if (result is not null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productVM);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
