deb40f86813004abefface2ddadf3f310f998d4a
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-14 21:29:39 -0300

    feat(Discount): add mapping

---
+++ EShop.DiscountApi/DTOs/Mapping/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using EShop.DiscountApi.Models;
+
+namespace EShop.DiscountApi.DTOs.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<CouponDTO, Coupon>().ReverseMap();
+    }
+}
