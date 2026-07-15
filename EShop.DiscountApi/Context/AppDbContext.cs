487eb6468f53e02f693778a66e440efb65cb1a59
Luiz Gustavo <gugajoao1020@gmail.com>
2026-07-14 21:33:29 -0300

    feat(Discount): add dbContext

---
+++ EShop.DiscountApi/Context/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using EShop.DiscountApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.DiscountApi.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Coupon> Coupons { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Coupon>().HasData(new Coupon
+        {
+            CouponId = 1,
+            CouponCode = "ESHOP_PROMO_10",
+            Discount = 10
+        });
+        modelBuilder.Entity<Coupon>().HasData(new Coupon
+        {
+            CouponId = 2,
+            CouponCode = "ESHOP_PROMO_20",
+            Discount = 20
+        });
+    }
+}
+
